493fd2a1b02bd7d2899494cb18b26a5ed78fa28a
wolfAC <anbu8132609@gmail.com>
2026-08-02 19:33:33 +0530

    Empty repo to start fresh with new portfolio
    Removing all existing files while keeping git history and the remote intact.

--- sources/data/constants.js
+++
@@ -1,9 +0,0 @@
-// Central place for the branding strings that used to be hardcoded as
-// "Bruno Simon" throughout the project. Edit these to rebrand -- the tab
-// title, console signature, and social links all read from here. index.html
-// is plain static markup (not a JS module), so its title/meta tags and the
-// home bio card can't import this file directly; keep those in sync by hand.
-export const AUTHOR_NAME = 'Wolf'
-export const SITE_TITLE = `${AUTHOR_NAME}'s`
-export const SITE_DESCRIPTION = `${AUTHOR_NAME}'s creative portfolio`
-export const REPO_URL = 'https://github.com/wolfAC/wolf-portfolio'
